0964478eb137e139aed91cd77526c8bda0376958
modessam <64746678+modessam@users.noreply.github.com>
2023-05-17 21:07:34 +0300

    Update ports.c moD

--- ports.c
+++ ports.c
@@ -4,53 +4,30 @@
 #include "timers.h"
 
 volatile unsigned long delay;
-unsigned char Sw1Flag = 0, Sw2Flag = 0;
 
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
 void PortB_Init(void)
 {
-
+//17-> 1 in regester so it is in enable0 bit 1 and in priority0 in INTB
   SYSCTL_RCGC2_R |= 0x00000002;   // 1) B clock
   delay = SYSCTL_RCGCGPIO_R;      // delay
   GPIO_PORTB_LOCK_R = 0x4C4F434B; // 2)unlock GPIO of PORTB
   GPIO_PORTB_CR_R = 0x01;         // Enable commit
   GPIO_PORTB_AMSEL_R = 0x00;      // 3) disable analog function
   GPIO_PORTB_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
-  GPIO_PORTB_DIR_R = 0xFF;        // 5) PORT output
+  GPIO_PORTB_DIR_R = 0xEE;        // 5) PORT output for all pins except pins 0 & 4 
   GPIO_PORTB_AFSEL_R = 0x00;      // 6) no alternate function
   GPIO_PORTB_DEN_R = 0xFF;        // 7) enable digital pins PF4-PF0
-}
-
-/*void PortF_Init(void)
-{
-  SYSCTL_RCGC2_R |= 0x00000020;   // 1) F clock
-  delay = SYSCTL_RCGC2_R;         // delay
-  GPIO_PORTF_LOCK_R = 0x4C4F434B; // 2) unlock PortF PF0
-  GPIO_PORTF_CR_R = 0x1F;         // allow changes to PF4-0
-  GPIO_PORTF_AMSEL_R = 0x00;      // 3) disable analog function
-  GPIO_PORTF_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
-  GPIO_PORTF_DIR_R = 0x0E;        // 5) PF4,PF0 input, PF3,PF2,PF1 output
-  GPIO_PORTF_AFSEL_R = 0x00;      // 6) no alternate function
-  GPIO_PORTF_PUR_R = 0x11;        // enable pullup resistors on PF4,PF0
-  GPIO_PORTF_DEN_R = 0x1F;        // 7) enable digital pins PF4-PF0
-  GPIO_PORTF_IS_R &= ~0x11;       // (d) PF4, PF0 are edge-sensitive
+	GPIO_PORTF_PUR_R = 0x11;        // enable pullup resistors on PF4,PF0  
+	GPIO_PORTF_IS_R &= ~0x11;       // (d) PF4, PF0 are edge-sensitive
   GPIO_PORTF_IBE_R &= ~0x11;      // PF4, PF0 are not both edges
   GPIO_PORTF_IEV_R &= ~0x11;      // PF4, Pf0 are falling edge event
   GPIO_PORTF_ICR_R = 0x11;        // (e) Clear flag4, flag0
   GPIO_PORTF_IM_R |= 0x11;        // (f) Arm interrupt on PF4, PF0
-  NVIC_PRI7_R = (NVIC_PRI7_R & 0xff00ffff) | 0x00A00000;
-  NVIC_EN0_R = 0x40000000;
-  EnableInterrupts();
-}*/
+	NVIC_PRI0_R = (NVIC_PRI7_R & 0xffff8fff) | 0x00000000;
+  NVIC_EN0_R = 0x00000002;
+	EnableInterrupts();
+}
+
 void PortF_Init(void){ volatile unsigned long delay;
   SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
   delay = SYSCTL_RCGC2_R;           // delay   
