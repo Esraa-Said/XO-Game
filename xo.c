bd00072aa43478842bd7147996483d11a5129f8d
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2023-05-19 03:40:31 +0300

    fix: port.c

--- xo.c
+++ xo.c
@@ -17,13 +17,10 @@ void reset_timer0(void);
 
 int pos, time = 1;
 
-
 // check winner  0  draw   1 x     2  o   3 no action
 // *************************** Images ***************************
 // enemy ship that starts at the top of the screen (arms/mouth closed)
 // width=16 x height=10
-
-
 
 void splashScreen(void);
 void game_over(void);
@@ -65,9 +62,11 @@ int main(void){
 	TExaS_Init(SSI0_Real_Nokia5110_Scope);  // set system clock to 80 MHz
   Nokia5110_Init();
   
-	
+	GPIO_PORTB_DATA_R |= 0x80;
 	splashScreen();
-	Delay100ms(5);
+	Delay100ms(20);
+	
+	GPIO_PORTB_DATA_R &= ~(0x80);
 	
 	//Timer0_Init(5 * 160000000);//1 sec timer
   Timer2_Init(160000000);//1 sec timer
@@ -80,7 +79,43 @@ int main(void){
 	SW2=0;
 	while(1){
 
-		if (ok)break;	
+		while (ok)
+		{
+			int i, j;
+			
+			
+			//ok = 0;
+			
+				
+				
+			
+				if(SW1 && !SW2){                // just SW1 pressed
+						pos = 0;
+						for ( i = 0; i < 3; i++) {
+							for (j = 0; j < 3; j++)
+							{
+									arr[i][j] = 0;
+							}
+						}
+						arr[0][0] = 3;
+						turn = 0;
+						SW1=0;
+						SW2=0;
+						TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+						time = 1;
+						ok = 0;
+						GPIO_PORTB_DATA_R &= ~(0x80);
+						turnX();
+				}
+				else if(SW2 && !SW1){              // just SW2 pressed
+					Nokia5110_Clear();
+					GPIO_PORTB_DATA_R &= ~(0x80);			
+					Nokia5110_SetCursor(1, 1);
+					Nokia5110_OutString("t3ala kol youm");
+					return 0;
+				}	
+			
+			}	
 		//SW1 = GPIO_PORTF_DATA_R&0x10;     // read PF4 into SW1
     		//SW2 = GPIO_PORTF_DATA_R&0x01;     // read PF0 into SW2
 	if(SW2 && !SW1){                // just SW2 pressed
@@ -98,12 +133,33 @@ int main(void){
 			//Timer0_Init(5 * 160000000);//1 sec timer
 			//Timer2_Init(160000000);//1 sec timer
 			reset_timer2();
+		
+			/*GPIO_PORTB_DATA_R |= 0x40; 
+			Delay100ms(2);
+			GPIO_PORTB_DATA_R &= ~(0x40);*/
 			//reset_timer0();
 			//Delay100ms(5);
 			time = 1;
-			if(update())break;
-			if(turn)turnO();
-			else turnX();
+			if(update()){
+				TIMER2_CTL_R &= ~(1 << 0); // Clear bit 0 to disable Timer2A
+				Delay100ms(20);
+				Nokia5110_Clear();
+				GPIO_PORTB_DATA_R &= ~(0x08);
+				GPIO_PORTB_DATA_R &= ~(0x04);
+				GPIO_PORTB_DATA_R |= 0x80;				
+				Nokia5110_SetCursor(1, 1);
+				Nokia5110_OutString("SW1 to play again");
+				Nokia5110_SetCursor(1, 4);
+				Nokia5110_OutString("SW2 To end");
+				SW1 = 0;
+				SW2 = 0;
+				ok = 1;
+			}
+			else 
+			{
+				if(turn)turnO();
+				else turnX();	
+			}
 			//PortB_Init();
 			//Delay100ms(2);
 			
@@ -221,6 +277,8 @@ void Timer0A_Handler(void){
 void turnO(void)
 {
 		startGame();
+	GPIO_PORTB_DATA_R &= ~(0x04);
+	GPIO_PORTB_DATA_R |= 0x08;  
 	Nokia5110_PrintBMP(3 * SQUAREHEIGHT  + 9, (VH - 1), VLine, 0);
 	Nokia5110_DisplayBuffer();
 	 Nokia5110_SetCursor(8, 2);	
@@ -234,7 +292,8 @@ void turnX(void)
 {
 	
 	startGame();
-
+	GPIO_PORTB_DATA_R &= ~(0x08);
+	GPIO_PORTB_DATA_R |= 0x04;  
 	 Nokia5110_PrintBMP(3 * SQUAREHEIGHT  + 9, (VH - 1), VLine, 0);
 	 Nokia5110_DisplayBuffer();
 	
@@ -338,17 +397,17 @@ int update(){
 		
 		if(w==1)
 		{
-			GPIO_PORTF_DATA_R=0x04; 
+			//GPIO_PORTF_DATA_R=0x04; 
 			winner_x();
 		}
 		else if(w==2)
 		{
-			GPIO_PORTF_DATA_R=0x08; 
+			//GPIO_PORTF_DATA_R = 0x08; 
 			winner_o();
 		}
 		else if(w==3)
 		{
-				GPIO_PORTF_DATA_R=0x0F; 
+				//GPIO_PORTF_DATA_R=0x0F; 
 				game_over();
 		}
 		
@@ -426,12 +485,18 @@ void splashScreen()
 
 void game_over() {
 	Nokia5110_Clear();
+	GPIO_PORTB_DATA_R |= 0x80;
+	GPIO_PORTB_DATA_R &= ~(0x04);
+	GPIO_PORTB_DATA_R &= ~(0x08); 
   Nokia5110_SetCursor(2, 2);
   Nokia5110_OutString("DRAW");
 }
 
 void winner_x() {
 	Nokia5110_Clear();
+	GPIO_PORTB_DATA_R &= ~(0x08);
+	GPIO_PORTB_DATA_R |= 0x04;  
+	GPIO_PORTB_DATA_R &= ~(0x80); 
   Nokia5110_SetCursor(1, 1);
   Nokia5110_OutString("THE WINNER");
   Nokia5110_SetCursor(4, 3);
@@ -440,6 +505,9 @@ void winner_x() {
 
 void winner_o() {
 	Nokia5110_Clear();
+	GPIO_PORTB_DATA_R &= ~(0x04);
+	GPIO_PORTB_DATA_R |= 0x08; 
+	GPIO_PORTB_DATA_R &= ~(0x80); 
   Nokia5110_SetCursor(1, 1);
   Nokia5110_OutString("THE WINNER");
   Nokia5110_SetCursor(4, 3);
